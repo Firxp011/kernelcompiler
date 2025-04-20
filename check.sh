2c076d2bee872b753dfcd9a80c5e15ff575daefd
Firxp011 <rozlifirdaus9@gmail.com>
2025-04-20 10:42:39 +0000

    add script to branch

---
+++ check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo ================================================
+echo Please wait until the script is finished . . .
+echo Starting Check Script . . .
+echo The kernel source are ready to compile
+echo ================================================
+echo BUILDER NAME = ${KBUILD_BUILD_USER}
+echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
+echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
+echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
+echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
+echo AARCH64_ROOTDIR = ${AARCH64_ROOTDIR}
+echo ARM_ROOTDIR = ${ARM_ROOTDIR}
+echo ================================================
---
+++ target.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+KERNEL_ROOTDIR=~/ksu-next-dev/android_kernel_sony_sdm845
+DEVICE_DEFCONFIG=tama_akari_defconfig
+CLANG_ROOTDIR=~/toolchains/clang-r428724
+AARCH64_ROOTDIR=~/toolchains/aarch64-linux-android-4.9
+ARM_ROOTDIR=~/toolchains/arm-linux-androideabi-4.9
+export KBUILD_BUILD_USER=firxp011
+export KBUILD_BUILD_HOST=lycra

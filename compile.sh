95fe67ecd40b8ebea5288461315be2e32b8bb233
FIRxp011 <rozlifirdaus9@gmail.com>
2025-06-20 08:21:49 +0000

    kernelcompiler: add gitpod support
    Signed-off-by: FIRxp011 <rozlifirdaus9@gmail.com>

--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ cd ${KERNEL_ROOTDIR}
 make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
 	CC=${CLANG_ROOTDIR}/bin/clang \
-	CLANG_TRIPLE=${AARCH64_ROOTDIR}/bin/aarch64-linux-gnu- \
+	CLANG_TRIPLE=${CLANG_TRIPLE_ROOTDIR}/bin/aarch64-linux-gnu- \
 	CROSS_COMPILE=${AARCH64_ROOTDIR}/bin/aarch64-linux-android- \
 	CROSS_COMPILE_ARM32=${ARM_ROOTDIR}/bin/arm-linux-androideabi-
